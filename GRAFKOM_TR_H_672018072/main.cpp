ae2aee15c2ae80fc50f3303bccc421e6044746f0
ValZeta <valen.brata3@gmail.com>
2020-08-04 19:17:53 +0700

    Melanjutkan Pengerjaan main.cpp
    Pelanjutan main.cpp dan pemasangan texture

--- GRAFKOM_TR_H_672018072/main.cpp
+++ GRAFKOM_TR_H_672018072/main.cpp
@@ -2,13 +2,15 @@
 #include <GL/glut.h>
 #include <iostream>
 #include "NFG.h"
+#include "TGA.h"
+
 
 int w = 600, h = 600;
 static float ypoz = 0, zpoz = 0, xpoz = 0, a = 0, b = 0, c = 0;
 static float xScale = 1, yScale = 1, zScale = 1;
 
 float xrot = 0.0f;
-float yrot = 0.0f;
+float yrot = 180.0f;
 float xdiff = 0.0f;
 float ydiff = 0.0f;
 bool mouseDown = false;
@@ -57,74 +59,35 @@ void myKeyboard(unsigned char key, int x, int y) {
         yScale -= 0.1;
         zScale -= 0.1;
         break;
-    case 'x': // X axis
-        xpoz = xpoz + 5;
-        if (xpoz > 360) xpoz = 0;
-        glutPostRedisplay();
-        break;
-    case 'y': // Y axis
-        ypoz = ypoz + 5;
-        if (ypoz > 360) ypoz = 0;
-        glutPostRedisplay();
-        break;
-    case 'z': // Z axis
-        zpoz = zpoz + 1;
-        if (zpoz > 360) zpoz = 0;
-        glutPostRedisplay();
-        break;
-    case 'q': // Zoom in
-        c = c + 1;
-        glutPostRedisplay();
-        break;
-    case 'e': // Zoom out
-        c = c - 1;
-        glutPostRedisplay();
-        break;
     }
 }
 
 void processSpecialKeys(int key, int x, int y) {
     switch (key) {
     case GLUT_KEY_UP:
-        b = b + 1;
+        b = b + 0.2;
         glutPostRedisplay();
         break;
     case GLUT_KEY_DOWN:
-        b = b - 1;
+        b = b - 0.2;
         glutPostRedisplay();
         break;
     case GLUT_KEY_LEFT:
-        a = a - 1;
+        a = a - 0.2;
         glutPostRedisplay();
         break;
     case GLUT_KEY_RIGHT:
-        a = a + 1;
+        a = a + 0.2;
         glutPostRedisplay();
         break;
     }
 }
 
 void init() {
-    float ambientLight[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-    float diffuseLight[] = { 0.8f, 0.8f, 0.8f, 1.0f };
-    float specularLight[] = { 0.1f, 0.1f, 0.1f, 2.0f };
-    float lampPosition[] = { 0.0f, 1.0f, 0.0f, 1.0f };
-
-    glShadeModel(GL_SMOOTH);
+    glEnable(GL_DEPTH_TEST);
     glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
     glClearDepth(1.0f);
     glClearDepth(1.0f);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_DEPTH_TEST);
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
-    glLightfv(GL_LIGHT0, GL_POSITION, lampPosition);
-
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
     return;
 }
 
@@ -132,23 +95,25 @@ void renderScene(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(0, 0, 0, 1);
 
-    glLoadIdentity();
-    //gluLookAt(0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
     glPushMatrix();
     glTranslatef(0.0, -1.0, 0.0);
     glTranslatef(a, b, c);
 
-    //glScalef(xScale, yScale, zScale);
+    glScalef(xScale, yScale, zScale);
     glRotatef(xpoz, 1, 0, 0);
     glRotatef(ypoz, 0, 1, 0);
     glRotatef(zpoz, 0, 0, 1);
 
     glScalef(xScale, yScale, zScale);
-
     glRotatef(xrot, 1.0f, 0.0f, 0.0f);
     glRotatef(yrot, 0.0f, 1.0f, 0.0f);
     glColor3ub(255, 255, 255);
-    float x1 = 0, x2, x3, y1, y2, y3, z1, z2, z3;
+    float x1 = 0, x2, x3, y1, y2, y3, z1, z2, z3, u1, u2, u3, v1, v2, v3;
+    glEnable(GL_TEXTURE_2D);
+
+    glBindTexture(GL_TEXTURE_2D, _textureID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     for (int i = 0; i < 718; i++) {
 
         x1 = coorVertex[Sequence[i].i1].x;
@@ -160,10 +125,16 @@ void renderScene(void) {
         z1 = coorVertex[Sequence[i].i1].z;
         z2 = coorVertex[Sequence[i].i2].z;
         z3 = coorVertex[Sequence[i].i3].z;
-        glBegin(GL_LINE_LOOP);
-        glVertex3f(x1, y1, z1);
-        glVertex3f(x2, y2, z2);
-        glVertex3f(x3, y3, z3);
+        u1 = coorVertex[Sequence[i].i1].u;
+        u2 = coorVertex[Sequence[i].i2].u;
+        u3 = coorVertex[Sequence[i].i3].u;
+        v1 = coorVertex[Sequence[i].i1].v;
+        v2 = coorVertex[Sequence[i].i2].v;
+        v3 = coorVertex[Sequence[i].i3].v;
+        glBegin(GL_POLYGON);
+        glTexCoord2f(u1, v1); glVertex3f(x1, y1, z1);
+        glTexCoord2f(u2, v2); glVertex3f(x2, y2, z2);
+        glTexCoord2f(u3, v3); glVertex3f(x3, y3, z3);
         glEnd();
     }
     float d1[] = { 0.2, 0.5, 0.8, 1.0 };
@@ -180,9 +151,8 @@ void resize(int w1, int h1) {
     glViewport(0, 0, w1, h1);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    //gluPerspective(45.0, (float)w1 / (float)h1, 1.0, 300.0);
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -2.0, 2.0);
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
 }
 
 void timer(int value) {
@@ -196,8 +166,9 @@ int main(int argc, char** argv) {
     glutInitWindowPosition(100, 100);
     glutInitWindowSize(w, h);
     glutCreateWindow("TR_GRAFKOM_672018072_672018224_672018239");
-
+    init();
     load("Woman1.nfg");
+    LoadTGA_custom("Woman1.tga");
     glutDisplayFunc(renderScene);
     glutReshapeFunc(resize);
     glutKeyboardFunc(myKeyboard);
@@ -207,7 +178,5 @@ int main(int argc, char** argv) {
     glutIdleFunc(renderScene);
     glutSwapBuffers();
     glutTimerFunc(1, timer, 0);
-
-    init();
     glutMainLoop();
 }
